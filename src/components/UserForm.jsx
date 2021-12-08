5a2d7f39719f76247a0c05f1db03a326a4fd51b2
Trevor Anderson <tbone101@gmail.com>
2021-12-08 00:08:43 -0800

    Finished More Forms

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 
+const regexEmail = new RegExp(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
+const regexPassword = new RegExp(
+  /(?=^.{8,}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)[0-9a-zA-Z!@#$%^&*()]*$/
+);
+
 const UserForm = (props) => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
@@ -7,6 +12,11 @@ const UserForm = (props) => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState(false);
 
   const createUser = (e) => {
     e.preventDefault();
@@ -19,6 +29,52 @@ const UserForm = (props) => {
     setConfirmPassword("");
     setHasBeenSubmitted(true);
   };
+  const handleFirstName = (e) => {
+    setFirstName(e.target.value);
+    if (e.target.value.length < 2 && e.target.value.length > 0) {
+      setFirstNameError("First Name must be 2 or more characters");
+    } else {
+      setFirstNameError("");
+    }
+  };
+  const handleLastName = (e) => {
+    setLastName(e.target.value);
+    if (e.target.value.length < 2 && e.target.value.length > 0) {
+      setLastNameError("Last Name must be 2 or more characters");
+    } else {
+      setLastNameError("");
+    }
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    if (regexEmail.test(e.target.value)) {
+      setEmailError("");
+    } else if (e.target.value < 1) {
+      setEmailError("");
+    } else {
+      setEmailError("Email must be valid");
+    }
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    if (regexPassword.test(e.target.value)) {
+      setPasswordError("");
+    } else if (e.target.value < 1) {
+      setPasswordError("");
+    } else {
+      setPasswordError(
+        "Password must be more than 8 characters and contain 1 of Each of the following: Lowercase letter, Uppercase Letter, Number"
+      );
+    }
+  };
+  const handleConfirmPassword = (e) => {
+    setConfirmPassword(e.target.value);
+    if (e.target.value.length < 1 || e.target.value === password) {
+      setConfirmPasswordError("");
+    } else {
+      setConfirmPasswordError("Passwords Must Match");
+    }
+  };
   const formMessage = () => {
     if (hasBeenSubmitted) {
       return "Thank you for Submitting the form!";
@@ -28,7 +84,7 @@ const UserForm = (props) => {
   };
 
   return (
-    <div>
+    <div className="max-w-md">
       <form className="" onSubmit={createUser}>
         <h3 className="text-2xl font-bold">{formMessage()}</h3>
         <div className="flex justify-between">
@@ -37,45 +93,50 @@ const UserForm = (props) => {
             className="form-input appearance-none rounded border-double m-2 "
             type="text"
             value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
+            onChange={handleFirstName}
           />
         </div>
+        {firstNameError ? <p>{firstNameError}</p> : null}
         <div className="flex justify-between">
           <label className="pt-4">Last Name: </label>
           <input
             className="form-input appearance-none rounded border-double m-2 "
             type="text"
             value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
+            onChange={handleLastName}
           />
         </div>
+        {lastNameError ? <p>{lastNameError}</p> : null}
         <div className="flex justify-between">
           <label className="pt-4">Email Address: </label>
           <input
             className="form-input rounded m-2"
             value={email}
             type="text"
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={handleEmail}
           />
         </div>
+        {emailError ? <p>{emailError}</p> : null}
         <div className="flex justify-between">
           <label className="pt-4">Password: </label>
           <input
             className="form-input rounded m-2"
             value={password}
             type="password"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={handlePassword}
           />
         </div>
+        {passwordError ? <p className="break-words">{passwordError}</p> : null}
         <div className="flex justify-between">
           <label className="pt-4">Confirm Password: </label>
           <input
             className="form-input rounded m-2"
             value={confirmPassword}
             type="password"
-            onChange={(e) => setConfirmPassword(e.target.value)}
+            onChange={handleConfirmPassword}
           />
         </div>
+        {confirmPasswordError ? <p>{confirmPasswordError}</p> : null}
         <button
           className="w-28 border shadow-md rounded bg-gray-200 hover:bg-gray-100"
           type="submit"
